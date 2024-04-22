6b0d73babc2a2613c4c32268850cee0882901f8b
Lindsay Lee <yasdnil922@comcast.net>
2024-04-21 19:28:15 -0500

    Prepare table of vehicles to display

--- nashvillemta/calculate_eta.py
+++ nashvillemta/calculate_eta.py
@@ -1,13 +1,13 @@
 import googlemaps
 from datetime import datetime
 import pandas as pd
-from my_secrets import google_api_directions_key #objects with my secret info
+from my_secrets import google_api_directions_key, my_routes #objects with my secret info
 
 # Authenticate Google Maps API with key (restricted to Directions API)
 gmaps = googlemaps.Client(key=google_api_directions_key)
 
 # Load the vehicle data
-df_vehicles = pd.read_csv('temp_gtfs/vehicles_with_nearest_stops.csv')
+df_vehicles = pd.read_csv('temp_gtfs/vehicles_with_nearest_stops.csv', dtype={'route_id': str})
 
 def calculate_eta_transit(origin, destination, gmaps_client):
     now = datetime.now()
@@ -45,3 +45,15 @@ df_vehicles['eta_minutes'] = df_vehicles.apply(lambda row: calculate_eta_transit
 df_vehicles = df_vehicles.loc[~df_vehicles.has_passed][['route_id', 'direction_id', 'trip_id', 'vehicle_id', 'eta_minutes']].sort_values(['route_id', 'direction_id'])
 print(df_vehicles)
 
+#read in vehicle metadata
+vehicle_metadata = pd.read_csv('temp_gtfs/merged_data.csv', dtype={'route_id': str})[['route_id', 'direction_id', 'trip_id', 'trip_headsign', 'route_color', 'route_text_color']]
+
+#print rows of vehicle_metadata on my route
+print(vehicle_metadata.loc[vehicle_metadata['route_id'].isin(my_routes)])
+
+# Merge the vehicle data with the metadata
+vehicles_to_display = pd.merge(df_vehicles, vehicle_metadata, how='left', on=['route_id', 'direction_id', 'trip_id'])
+print(vehicles_to_display)
+
+# Save the data to a CSV file
+vehicles_to_display.to_csv('temp_gtfs/vehicles_to_display.csv', index=False)
